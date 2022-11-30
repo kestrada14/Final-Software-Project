e624ebd7d1b92f3af127ea3e6d8a3d61c0cd1827
Mateo Ortega <mateo041ortega@gmail.com>
2022-11-29 18:48:50 -0700

    Adding Database

---
+++ databasehandler.cpp
@@ -0,0 +1,36 @@
+#include "databasehandler.h"
+#include <QNetworkRequest>
+#include <QDebug>
+#include <QJsonDocument>
+#include <QVariantMap>
+
+DatabaseHandler::DatabaseHandler(QObject *parent)
+    : QObject(parent){
+    m_networkManager = new QNetworkAccessManager(this);
+    m_networkReply = m_networkManager->get(QNetworkRequest(QUrl("https://bank-9c1c4-default-rtdb.firebaseio.com/Client.json")));
+    connect(m_networkReply, &QNetworkReply::readyRead, this, &DatabaseHandler::networkReplyReadyRead);
+}
+
+DatabaseHandler::~DatabaseHandler()
+{
+    m_networkManager->deleteLater();
+}
+
+void DatabaseHandler::networkReplyReadyRead()
+{
+    qDebug() << m_networkReply->readAll();
+}
+
+void DatabaseHandler::createuser()
+{
+    QVariantMap newClient;
+    newClient["Email"] = "mateofoo@gmail.com";
+    newClient["Name"] = "Bar";
+    newClient["Phone Number"]= "911562353";
+    QJsonDocument jsonDoc = QJsonDocument:: fromVariant(newClient);
+    QNetworkRequest newClientRequestr(QUrl("https://bank-9c1c4-default-rtdb.firebaseio.com/Client.json"));
+    newClientRequestr.setHeader(QNetworkRequest::ContentTypeHeader, QString("application/json"));
+    m_networkManager->post(newClientRequestr,jsonDoc.toJson());
+
+    connect(m_networkReply, &QNetworkReply::readyRead, this, &DatabaseHandler::networkReplyReadyRead);
+}
---
+++ databasehandler.h
@@ -0,0 +1,28 @@
+#ifndef DATABASEHANDLER_H
+#define DATABASEHANDLER_H
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+
+
+class DatabaseHandler : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DatabaseHandler(QObject *parent = nullptr);
+    ~DatabaseHandler();
+
+ public slots:
+    void networkReplyReadyRead();
+    void createuser();
+signals:
+
+private:
+       QNetworkAccessManager * m_networkManager;
+        QNetworkReply * m_networkReply;
+};
+
+
+#endif // DATABASEHANDLER_H
